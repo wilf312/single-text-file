70a40c74497d4c53ae0e1f0064ec170b562ca86f
Okada Genya <g.okada.wilf+github@gmail.com>
2025-04-01 10:28:39 +0900

    logic: ディレクトリから特定の拡張子を掘ってマージする

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { join } from 'jsr:@std/path';
+
+export const endsWith = (str: string, suffix: string[] = ['.tsx', '.ts']) => {
+  return suffix.some((s) => str.endsWith(s));
+};
+
+async function mergeTextFiles(directory: string, outputFile: string) {
+  let mergedText = '';
+
+  for await (const entry of Deno.readDir(directory)) {
+    if (entry.isFile && endsWith(entry.name)) {
+      const filePath = join(directory, entry.name);
+      const text = await Deno.readTextFile(filePath);
+      mergedText += `/*----- ${entry.name} -----*/
+${text}`; // 各ファイルの内容を改行で区切って追加
+    }
+  }
+
+  await Deno.writeFile(outputFile, new TextEncoder().encode(mergedText));
+  console.log(`テキストファイルを ${outputFile} にまとめました。`);
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const directory = Deno.args[0];
+  const outputFile = Deno.args[1] || 'merged.txt';
+
+  if (!directory) {
+    console.error('使用方法: deno run merge_texts.ts <ディレクトリ> [出力ファイル名]');
+    Deno.exit(1);
+  }
+
+  await mergeTextFiles(directory, outputFile);
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,12 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { endsWith } from './main.ts';
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function _endsWith() {
+  assertEquals(endsWith('aaaa.ts'), true);
+  assertEquals(endsWith('aaaa.tsx'), true);
+  assertEquals(endsWith('aaaa'), false);
+
+  assertEquals(endsWith('aaaa', ['aaaa']), true);
+  assertEquals(endsWith('aaaa.ts', ['.ts', '.tsx']), true);
+  assertEquals(endsWith('aaaa.ts', []), false);
 });
